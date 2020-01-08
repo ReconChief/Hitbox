1e3a3d9401500eb66ae92e1c38b03a36be4594e6
ReconChief <32560933+ReconChief@users.noreply.github.com>
2020-01-08 15:00:37 -0500

    Game can now be navigated with Controller
    If players are using controllers, they can navigate through the menu just using the controllers and not have to use the mouse to navigate the game.

--- BoxersCapstone/Assets/Scripts/UI/TitleScreen.cs
+++ BoxersCapstone/Assets/Scripts/UI/TitleScreen.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
@@ -14,11 +15,20 @@ public class TitleScreen : MonoBehaviour
 
     public GameObject stageSelectScreen;
 
+    //Buttons For Next Screens
+    [Header("Buttons")]
+    public GameObject mainMenuButton;
+    public GameObject controlsButton;
+    public GameObject returnKeyButton;
+    public GameObject returnControllerButton;
+    public GameObject stageSelectButton;
+
     public void returnToTitle()
     {
         titleScreen.SetActive(true);
         controlScreen.SetActive(false);
         stageSelectScreen.SetActive(false);
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(mainMenuButton,null);
     }
 
     public void returnToControls()
@@ -26,30 +36,35 @@ public class TitleScreen : MonoBehaviour
         optionsScreen.SetActive(true);
         keyboardScreen.SetActive(false);
         controllerScreen.SetActive(false);
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(controlsButton, null);
     }
 
     public void Controls()
     {
         titleScreen.SetActive(false);
         controlScreen.SetActive(true);
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(controlsButton,null);
     }
 
     public void keyboardControls()
     {
         optionsScreen.SetActive(false);
         keyboardScreen.SetActive(true);
-    }
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(returnKeyButton, null);
+;    }
 
     public void controllerControls()
     {
         optionsScreen.SetActive(false);
         controllerScreen.SetActive(true);
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(returnControllerButton,null);
     }
 
     public void StageSelect()
     {
         titleScreen.SetActive(false);
         stageSelectScreen.SetActive(true);
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(stageSelectButton,null);
     }
 
     public void quitGame()
